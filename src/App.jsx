edaa8171c1a37dfa243eafb37d193bee63753483
najim <najim.rizky@jkt1.ebdesk.com>
2023-03-16 20:32:23 +0700

    feat: test commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,13 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} 
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <p>
+          (Halo ini Najim)
         </p>
       </div>
       <p className="read-the-docs">
