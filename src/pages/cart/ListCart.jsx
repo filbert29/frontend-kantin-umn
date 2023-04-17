72e6aaf18c2b615789e324cf1ed594b8906d67a5
filbert29 <fkhouwira70@gmail.com>
2023-04-17 14:38:38 +0700

    refactor: pindahin code

--- src/pages/cart/ListCart.jsx
+++ src/pages/cart/ListCart.jsx
@@ -1,9 +1,64 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import Header from "../../component/Header";
 import PicTenant from "../../assets/pic-tenant.png"
+import { Link } from "react-router-dom";
 
 const ListCart = () => {
     const title = "Cart"
+
+    const Liner = () => {
+        return (
+            <Box my={"30px"} sx={{ border: "1px solid #D6D6D6" }} />
+        )
+    }
+
+    const CardCart = ({ data }) => {
+        return (
+            <Box display={"grid"} justifyContent={"center"}>
+                <Box className="shadow-box"
+                    sx={{
+                        padding: "25px 30px",
+                        borderRadius: "10px",
+                        width: "600px"
+                    }}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "25px",
+                        }}>
+                        <img src={PicTenant} alt="" />
+                        <Box className="deskripsi" sx={{
+                            display: "grid"
+                        }}>
+                            <Typography variant="p" fontSize={"20px"} fontWeight={"bold"}>{data?.tenant}</Typography>
+                            <Typography variant="p" fontSize={"16px"}>{data?.foods}</Typography>
+                        </Box>
+                    </Box>
+                    <Box className="price-control" sx={{
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        alignItems: "center",
+                        gap: "10px"
+                    }}>
+                        <Typography variant="p" fontWeight={"bold"}>Total Price: Rp. {data?.price}</Typography>
+                        <Button component={Link} to={"/customer/order/orderconfirmation"} sx={{
+                            backgroundColor: "#3DA9FC",
+                            color: "white",
+                            fontFamily: "Poppins",
+                            ":hover": { backgroundColor: "#058ffa" }
+                        }}>Checkout</Button>
+                        <Button sx={{
+                            backgroundColor: "#EF4565",
+                            color: "white",
+                            fontFamily: "Poppins",
+                            ":hover": { backgroundColor: "#f00a35" }
+                        }}>Delete</Button>
+                    </Box>
+                </Box>
+            </Box>
+        )
+    }
 
     return (
         <div>
@@ -30,8 +85,6 @@ const ListCart = () => {
                     <CardCart data={{ tenant: "Kedai Nasi Goreng", foods: "Nasi Goreng, Jus jeruk", price: 23000 }} />
                     <Liner />
                     <CardCart data={{ tenant: "Kedai Nasi Goreng", foods: "Nasi Goreng, Jus jeruk", price: 23000 }} />
-                    <Liner />
-                    <CardCart data={{ tenant: "Kedai Nasi Goreng", foods: "Nasi Goreng, Jus jeruk", price: 23000 }} />
                 </Box>
             </Container >
         </div>
@@ -40,56 +93,3 @@ const ListCart = () => {
 
 export default ListCart;
 
-const Liner = () => {
-    return (
-        <Box my={"30px"} sx={{ border: "1px solid #D6D6D6" }} />
-    )
-}
-
-const CardCart = ({ data }) => {
-    return (
-        <Box display={"grid"} justifyContent={"center"}>
-            <Box className="shadow-box"
-                sx={{
-                    padding: "25px 30px",
-                    borderRadius: "10px",
-                    width: "600px"
-                }}>
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "25px",
-                    }}>
-                    <img src={PicTenant} alt="" />
-                    <Box className="deskripsi" sx={{
-                        display: "grid"
-                    }}>
-                        <Typography variant="p" fontSize={"20px"} fontWeight={"bold"}>{data?.tenant}</Typography>
-                        <Typography variant="p" fontSize={"16px"}>{data?.foods}</Typography>
-                    </Box>
-                </Box>
-                <Box className="price-control" sx={{
-                    display: "flex",
-                    justifyContent: "flex-end",
-                    alignItems: "center",
-                    gap: "10px"
-                }}>
-                    <Typography variant="p" fontWeight={"bold"}>Total Price: Rp. {data?.price}</Typography>
-                    <Button sx={{
-                        backgroundColor: "#3DA9FC",
-                        color: "white",
-                        fontFamily: "Poppins",
-                        ":hover": { backgroundColor: "#058ffa" }
-                    }}>Checkout</Button>
-                    <Button sx={{
-                        backgroundColor: "#EF4565",
-                        color: "white",
-                        fontFamily: "Poppins",
-                        ":hover": { backgroundColor: "#f00a35" }
-                    }}>Delete</Button>
-                </Box>
-            </Box>
-        </Box>
-    )
-}
