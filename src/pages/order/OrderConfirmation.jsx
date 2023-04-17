1c3ea2c588d492b59dda855557323a88be685898
filbert29 <fkhouwira70@gmail.com>
2023-04-17 12:41:20 +0700

    refactor: tidy up order confirmation jsx

--- src/pages/order/OrderConfirmation.jsx
+++ src/pages/order/OrderConfirmation.jsx
@@ -1,88 +1,11 @@
-import { Box, Button, Container, Stack, Step, StepConnector, StepLabel, Stepper, Typography, stepConnectorClasses } from "@mui/material"
+import { Box, Button, Container, Typography } from "@mui/material"
 import Header from "../../component/Header"
-import { styled } from '@mui/material/styles';
-import PropTypes from 'prop-types';
-import Check from "../../assets/check.png"
 import IconLocation from "../../assets/icon-location.png"
 import JusJeruk from "../../assets/jus-jeruk.png"
 import NasiGoreng from "../../assets/pic-food.png"
 
 function OrderConfirmation() {
   const title = "Order Confirmation"
-
-  const QontoConnector = styled(StepConnector)(({ theme }) => ({
-    [`&.${stepConnectorClasses.alternativeLabel}`]: {
-      top: 10,
-      left: 'calc(-50% + 16px)',
-      right: 'calc(50% + 16px)',
-    },
-    [`&.${stepConnectorClasses.active}`]: {
-      [`& .${stepConnectorClasses.line}`]: {
-        borderColor: '#357DED',
-      },
-    },
-    [`&.${stepConnectorClasses.completed}`]: {
-      [`& .${stepConnectorClasses.line}`]: {
-        borderColor: '#357DED',
-      },
-    },
-    [`& .${stepConnectorClasses.line}`]: {
-      borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
-      borderTopWidth: 3,
-      borderRadius: 1,
-    },
-  }));
-
-  const QontoStepIconRoot = styled('div')(({ theme, ownerState }) => ({
-    color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#eaeaf0',
-    display: 'flex',
-    height: 22,
-    alignItems: 'center',
-    ...(ownerState.active && {
-      color: '#357DED',
-    }),
-    '& .QontoStepIcon-completedIcon': {
-      color: '#357DED',
-      zIndex: 1,
-      fontSize: 18,
-    },
-    '& .QontoStepIcon-circle': {
-      width: 8,
-      height: 8,
-      borderRadius: '50%',
-      backgroundColor: 'currentColor',
-    },
-  }));
-
-  function QontoStepIcon(props) {
-    const { active, completed, className } = props;
-
-    return (
-      <QontoStepIconRoot ownerState={{ active }} className={className}>
-        {completed ? (
-          <img src={Check} alt="" width={35} />
-        ) : (
-          <div className="QontoStepIcon-circle" />
-        )}
-      </QontoStepIconRoot>
-    );
-  }
-
-  QontoStepIcon.propTypes = {
-    /**
-     * Whether this step is active.
-     * @default false
-     */
-    active: PropTypes.bool,
-    className: PropTypes.string,
-    /**
-     * Mark the step as completed. Is passed to child components.
-     * @default false
-     */
-    completed: PropTypes.bool,
-  };
-
-  const steps = ['Sedang menunggu konfirmasi', 'Makanan sedang dibuat', 'Siap disajikan', 'Selesai'];
 
   return (
     <Container
@@ -140,7 +63,7 @@ function OrderConfirmation() {
             gap: "30px"
           }}>
             <CardDetailMakanan menu={{ title: "Nasi Goreng", description: "nasi goreng dari kampung", image: NasiGoreng, price: 15000 }} />
-            <CardDetailMakanan menu={{ title: "Jus Jeruk", description: "minuman dari jeruk", image: NasiGoreng, price: 15000 }} />
+            <CardDetailMakanan menu={{ title: "Jus Jeruk", description: "minuman dari jeruk", image: JusJeruk, price: 15000 }} />
           </Box>
         </Box>
         <Box mt={"50px"} display={"flex"}>
