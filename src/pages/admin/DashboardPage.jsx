8daf54347466965dcc663a073d55880837814c1c
najim <najim.rizky@jkt1.ebdesk.com>
2023-04-25 22:05:49 +0700

    feat: adjust dashboard page

--- src/pages/admin/DashboardPage.jsx
+++ src/pages/admin/DashboardPage.jsx
@@ -1,8 +1,19 @@
+import { Box, Divider, Typography } from "@mui/material";
+
 const DashboardPage = () => {
     return (
-        <h1>
-            DashboardPage
-        </h1>
+        <Box textAlign={"center"} py={10}>
+            <Typography variant="h4" component="h1" fontWeight={"bold"}>
+                Hello Admin!
+            </Typography>
+
+            
+            <Divider sx={{ my: 2 }} />
+
+            <Typography variant="h5" component="p">
+                Welcome to the admin dashboard. This is where you can manage kantin UMN data
+            </Typography>
+        </Box>
     );
 }
 
