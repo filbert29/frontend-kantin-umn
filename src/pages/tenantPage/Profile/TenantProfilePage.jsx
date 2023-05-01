ffd66258776a31823711ebb75209fd58031b4a12
najim <najim.rizky@jkt1.ebdesk.com>
2023-05-01 18:10:43 +0700

    feat: add logout button on TenantProfilePage

--- src/pages/tenantPage/Profile/TenantProfilePage.jsx
+++ src/pages/tenantPage/Profile/TenantProfilePage.jsx
@@ -1,9 +1,21 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { setLogout } from "../../../store/Auth";
+import { Logout } from "@mui/icons-material";
 
 const TenantProfilePage = () => {
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch(setLogout())
+    }
+
     return (
         <Box>
             <Typography variant="h4" sx={{ textAlign: "center" }}>Profile Page</Typography>
+            <Button onClick={handleLogout} variant="contained" endIcon={<Logout />}>
+                Logout
+            </Button>
         </Box>
     );
 }
