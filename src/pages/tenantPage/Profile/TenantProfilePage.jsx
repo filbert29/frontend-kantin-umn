01e8ca23d5881c2335f82c86bdd5803b2091d69b
najim <najim.rizky@jkt1.ebdesk.com>
2023-05-06 08:08:29 +0700

    feat: add change password for tenant

--- src/pages/tenantPage/Profile/TenantProfilePage.jsx
+++ src/pages/tenantPage/Profile/TenantProfilePage.jsx
@@ -1,14 +1,14 @@
 import { Box, Button, CircularProgress, Container, IconButton, Modal, TextField, Typography } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { setLogout } from "../../../store/Auth";
-import { Edit, Logout, Upload } from "@mui/icons-material";
+import { Edit, Lock, Logout, Upload } from "@mui/icons-material";
 import { useSelector } from "react-redux";
 import useSWR from "swr";
 import BASE_URL from "../../../config/BASE_URL";
 import Loading from "../../../component/state/Loading";
 import ErrorApi from "../../../component/state/ErrorApi";
 import fetcher from "../../../helper/fetcher";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ModalStyle } from "../../admin/Tenant/TenantDetailPage";
 import axios from "axios";
 import { addNotification } from "../../../store/Notification";
@@ -26,6 +26,7 @@ const TenantProfilePage = () => {
 
     const [openEditProfileImage, setOpenEditProfileImage] = useState(false)
     const [openEditProfile, setOpenEditProfile] = useState(false)
+    const [openChangePassword, setOpenChangePassword] = useState(false)
 
     if (isLoading || isValidating) return <Loading />
     if (error) return <ErrorApi />
@@ -75,6 +76,9 @@ const TenantProfilePage = () => {
                         <Typography variant="h5" fontSize={20}>{profile?.location}</Typography>
                     </Box>
                 </Box>
+                <Button onClick={() => setOpenChangePassword(true)} variant="contained" color="warning" endIcon={<Lock />}>
+                    Change Password
+                </Button>
                 <Button onClick={handleLogout} variant="contained" endIcon={<Logout />}>
                     Logout
                 </Button>
@@ -90,6 +94,10 @@ const TenantProfilePage = () => {
                 mutate={mutate}
                 handleClose={() => { setOpenEditProfile(false) }}
                 profile={profile}
+            />}
+            {openChangePassword && <ModalChangePassword
+                open={openChangePassword}
+                handleClose={() => { setOpenChangePassword(false) }}
             />}
         </>
     );
@@ -153,7 +161,7 @@ const ModalEditProfileImage = ({ open, mutate, handleClose }) => {
 
     return (
         <Modal open={open} onClose={onClose}>
-            <Box sx={{ ...ModalStyle, width: {xs: "280px", sm: "400px"} }}>
+            <Box sx={{ ...ModalStyle, width: { xs: "280px", sm: "400px" } }}>
                 {!imagePreview ? (
                     <Box
                         onClick={handleClickEditImage}
@@ -233,7 +241,7 @@ const ModalEditProfile = ({ open, mutate, handleClose, profile }) => {
                 onClose()
             }
         } catch (error) {
-            setFormError(error?.message)
+            setFormError(error?.response?.data?.error)
         } finally {
             setLoading(false)
         }
@@ -245,10 +253,11 @@ const ModalEditProfile = ({ open, mutate, handleClose, profile }) => {
 
     return (
         <Modal open={open} onClose={onClose}>
-            <Box sx={{ ...ModalStyle, width: {xs: "280px", sm: "400px"} }}>
+            <Box sx={{ ...ModalStyle, width: { xs: "280px", sm: "400px" } }}>
                 <Box sx={{ display: "grid", rowGap: 2 }}>
                     <TextField
                         fullWidth
+                        type="text"
                         label="Full Name"
                         variant="outlined"
                         name="full_name"
@@ -257,6 +266,7 @@ const ModalEditProfile = ({ open, mutate, handleClose, profile }) => {
                     />
                     <TextField
                         fullWidth
+                        type="text"
                         label="Location"
                         variant="outlined"
                         name="location"
@@ -266,6 +276,7 @@ const ModalEditProfile = ({ open, mutate, handleClose, profile }) => {
                     <TextField
                         fullWidth
                         label="Email"
+                        type="text"
                         variant="outlined"
                         name="email"
                         value={form.email}
@@ -289,6 +300,107 @@ const ModalEditProfile = ({ open, mutate, handleClose, profile }) => {
                         {loading ? <CircularProgress size={16} /> : "Submit"}
                     </Button>
                 </Box>
+            </Box>
+        </Modal>
+    )
+}
+
+const ModalChangePassword = ({ open, handleClose }) => {
+    const { access_token } = useSelector((state) => state.auth.accountData)
+    const [loading, setLoading] = useState(false)
+    const [formError, setFormError] = useState()
+    const [isFormValid, setIsFormValid] = useState(false)
+    const [form, setForm] = useState({
+        password: "",
+        new_password: "",
+        confirm_new_password: ""
+    })
+
+    const dispatch = useDispatch()
+
+    const handleChangeForm = (e) => {
+        setFormError(undefined)
+        setForm((prevState) => ({ ...prevState, [e.target.name]: e.target.value }))
+    }
+
+    useEffect(() => {
+        if (form.password.length >= 6 && form.new_password.length >= 6 && form.confirm_new_password.length >= 6) {
+            if (form.new_password !== form.confirm_new_password) {
+                setIsFormValid(false)
+            } else {
+                setIsFormValid(true)
+            }
+        } else {
+            setIsFormValid(false)
+        }
+    }, [form])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            setLoading(true)
+            const response = await axios.put(`${BASE_URL}/tenant/change-password`, form, {
+                headers: {
+                    Authorization: `Bearer ${access_token}`
+                }
+            })
+
+            if (response?.status === 200) {
+                dispatch(addNotification({ message: "Success edit profile", type: "success" }))
+                handleClose()
+            }
+        } catch (error) {
+            setFormError(error?.response?.data?.error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <Modal open={open} onClose={handleClose}>
+
+            <Box sx={{ ...ModalStyle, width: { xs: "280px", sm: "400px" } }}>
+                <form onSubmit={handleSubmit}>
+                    <Box sx={{ display: "grid", rowGap: 2 }}>
+                        <TextField
+                            fullWidth
+                            required
+                            label="Password"
+                            variant="outlined"
+                            name="password"
+                            value={form.password}
+                            onChange={handleChangeForm}
+                            type="password"
+                        />
+                        <TextField
+                            fullWidth
+                            required
+                            label="New Password"
+                            variant="outlined"
+                            name="new_password"
+                            value={form.new_password}
+                            onChange={handleChangeForm}
+                            type="password"
+                        />
+                        <TextField
+                            fullWidth
+                            required
+                            label="Confirm New Password"
+                            variant="outlined"
+                            name="confirm_new_password"
+                            value={form.confirm_new_password}
+                            onChange={handleChangeForm}
+                            type="password"
+                        />
+                    </Box>
+                    {formError && <Typography color="red">{formError}</Typography>}
+                    <Box sx={{ display: "flex", justifyContent: "center", mt: 3 }}>
+                        <Button fullWidth color="error" onClick={handleClose} variant="contained" sx={{ mr: 2 }}>Cancel</Button>
+                        <Button fullWidth disabled={loading === true || !isFormValid} type="submit" variant="contained">
+                            {loading ? <CircularProgress size={16} /> : "Submit"}
+                        </Button>
+                    </Box>
+                </form>
             </Box>
         </Modal>
     )
