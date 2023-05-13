eb013d393372f1138af65a71af20154563e73f6f
filbert29 <fkhouwira70@gmail.com>
2023-05-13 14:49:58 +0700

    fix: rename file store/Store

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import { store } from './store/Store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
