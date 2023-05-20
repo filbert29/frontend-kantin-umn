a4904e61c05cc0e6d8e2bcdc63c40489211389c6
najim <najim.rizky@jkt1.ebdesk.com>
2023-05-21 06:59:58 +0700

    feat: add error handling at browserNotification

--- src/helper/browserNotification.js
+++ src/helper/browserNotification.js
@@ -1,14 +1,18 @@
 const browserNotification = (message) => {
-    if (!("Notification" in window)) {
-        alert("This browser does not support desktop notification");
-    } else if (Notification.permission === "granted") {
-        new Notification(message);
-    } else if (Notification.permission !== "denied") {
-        Notification.requestPermission().then((permission) => {
-            if (permission === "granted") {
-                new Notification(message);
-            }
-        });
+    try {
+        if (!("Notification" in window)) {
+            alert("This browser does not support desktop notification");
+        } else if (Notification.permission === "granted") {
+            new Notification(message);
+        } else if (Notification.permission !== "denied") {
+            Notification.requestPermission().then((permission) => {
+                if (permission === "granted") {
+                    new Notification(message);
+                }
+            });
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
 
